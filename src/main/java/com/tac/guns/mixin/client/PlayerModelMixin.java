484e875a63a9e0db885cfaf0a3852620ae5e035f
tartaric_acid <baka943@qq.com>
2024-02-27 00:45:08 +0800

    添加 mixin 设计

---
+++ src/main/java/com/tac/guns/mixin/client/PlayerModelMixin.java
@@ -0,0 +1,132 @@
+package com.tac.guns.mixin.client;
+
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+/**
+ * Author: MrCrayfish
+ */
+@Mixin(PlayerModel.class)
+public class PlayerModelMixin<T extends LivingEntity> extends HumanoidModel<T> {
+    @Shadow
+    @Final
+    private boolean slim;
+
+    @Shadow
+    @Final
+    public ModelPart leftSleeve;
+
+    @Shadow
+    @Final
+    public ModelPart rightSleeve;
+
+    @Shadow
+    @Final
+    public ModelPart leftPants;
+
+    @Shadow
+    @Final
+    public ModelPart rightPants;
+
+    @Shadow
+    @Final
+    public ModelPart jacket;
+
+    public PlayerModelMixin(ModelPart part) {
+        super(part);
+    }
+
+    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At(value = "HEAD"))
+    private void setRotationAnglesHead(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        if (!(entityIn instanceof Player))
+            return;
+
+        this.resetRotationAngles();
+    }
+
+    @Inject(method = "setupAnim(Lnet/minecraft/world/entity/LivingEntity;FFFFF)V", at = @At(value = "TAIL"))
+    private void setRotationAnglesTail(T entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, CallbackInfo ci) {
+        if (!(entityIn instanceof Player))
+            return;
+        this.setupRotationAngles();
+    }
+
+    private void setupRotationAngles() {
+        this.rightSleeve.copyFrom(this.rightArm);
+        this.leftSleeve.copyFrom(this.leftArm);
+        this.rightPants.copyFrom(this.rightLeg);
+        this.leftPants.copyFrom(this.leftLeg);
+        this.jacket.copyFrom(this.body);
+        this.hat.copyFrom(this.head);
+    }
+
+    /**
+     * Resets all the rotations and rotation points back to their initial values. This makes it
+     * so ever developer doesn't have to do it themselves.
+     */
+    private void resetRotationAngles() {
+        this.resetAll(this.head);
+        this.resetAll(this.hat);
+        this.resetAll(this.body);
+        this.resetAll(this.jacket);
+
+        this.resetAll(this.rightArm);
+        this.rightArm.x = -5.0F;
+        this.rightArm.y = this.slim ? 2.5F : 2.0F;
+        this.rightArm.z = 0.0F;
+
+        this.resetAll(this.rightSleeve);
+        this.rightSleeve.x = -5.0F;
+        this.rightSleeve.y = this.slim ? 2.5F : 2.0F;
+        this.rightSleeve.z = 10.0F;
+
+        this.resetAll(this.leftArm);
+        this.leftArm.x = 5.0F;
+        this.leftArm.y = this.slim ? 2.5F : 2.0F;
+        this.leftArm.z = 0.0F;
+
+        this.resetAll(this.leftSleeve);
+        this.leftSleeve.x = 5.0F;
+        this.leftSleeve.y = this.slim ? 2.5F : 2.0F;
+        this.leftSleeve.z = 0.0F;
+
+        this.resetAll(this.leftLeg);
+        this.leftLeg.x = 1.9F;
+        this.leftLeg.y = 12.0F;
+        this.leftLeg.z = 0.0F;
+
+        this.resetAll(this.leftPants);
+        this.leftPants.copyFrom(this.leftLeg);
+
+        this.resetAll(this.rightLeg);
+        this.rightLeg.x = -1.9F;
+        this.rightLeg.y = 12.0F;
+        this.rightLeg.z = 0.0F;
+
+        this.resetAll(this.rightPants);
+        this.rightPants.copyFrom(this.rightLeg);
+    }
+
+    /**
+     * Resets the rotation angles and points to zero for the given model renderer
+     *
+     * @param part the model part to reset
+     */
+    private void resetAll(ModelPart part) {
+        part.xRot = 0.0F;
+        part.yRot = 0.0F;
+        part.zRot = 0.0F;
+        part.x = 0.0F;
+        part.y = 0.0F;
+        part.z = 0.0F;
+    }
+}
