7d45bf854712a8868a2c2c00368676118c104779
tartaric_acid <baka943@qq.com>
2024-03-02 13:22:59 +0800

    添加读取相关指令

---
+++ src/main/java/com/tac/guns/init/CommandRegistry.java
@@ -0,0 +1,14 @@
+package com.tac.guns.init;
+
+import com.tac.guns.command.RootCommand;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public final class CommandRegistry {
+    @SubscribeEvent
+    public static void onServerStaring(RegisterCommandsEvent event) {
+        RootCommand.register(event.getDispatcher());
+    }
+}
