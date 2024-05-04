126b5ae720d713f8e470ff10a99252fa46a639a0
tartaric_acid <baka943@qq.com>
2024-05-04 23:41:28 +0800

    完成配件的文本提示

--- src/main/java/com/tacz/guns/item/AttachmentItem.java
+++ src/main/java/com/tacz/guns/item/AttachmentItem.java
@@ -8,6 +8,9 @@ import com.tacz.guns.client.resource.index.ClientAttachmentIndex;
 import com.tacz.guns.init.ModItems;
 import com.tacz.guns.item.nbt.AttachmentItemDataAccessor;
 import com.tacz.guns.resource.index.CommonAttachmentIndex;
+import com.tacz.guns.resource.pojo.data.attachment.AttachmentData;
+import com.tacz.guns.resource.pojo.data.attachment.RecoilModifier;
+import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.core.NonNullList;
@@ -17,11 +20,16 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.IItemRenderProperties;
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.Nullable;
 
 import javax.annotation.Nonnull;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -75,5 +83,69 @@ public class AttachmentItem extends Item implements AttachmentItemDataAccessor {
         } else {
             return AttachmentType.NONE;
         }
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag isAdvanced) {
+        ResourceLocation attachmentId = getAttachmentId(stack);
+        AttachmentType type = getType(stack);
+        TimelessAPI.getClientAttachmentIndex(attachmentId).ifPresent(index -> {
+            AttachmentData data = index.getData();
+
+            if (type == AttachmentType.SCOPE) {
+                float[] zoom = index.getZoom();
+                if (zoom != null) {
+                    String[] zoomText = new String[zoom.length];
+                    for (int i = 0; i < zoom.length; i++) {
+                        zoomText[i] = "x" + zoom[i];
+                    }
+                    String zoomJoinText = StringUtils.join(zoomText, ", ");
+                    components.add(new TranslatableComponent("tooltip.tacz.attachment.zoom", zoomJoinText).withStyle(ChatFormatting.GOLD));
+                }
+            }
+
+            if (type == AttachmentType.EXTENDED_MAG) {
+                int magLevel = data.getExtendedMagLevel();
+                if (magLevel == 1) {
+                    components.add(new TranslatableComponent("tooltip.tacz.attachment.extended_mag_level_1").withStyle(ChatFormatting.GRAY));
+                } else if (magLevel == 2) {
+                    components.add(new TranslatableComponent("tooltip.tacz.attachment.extended_mag_level_2").withStyle(ChatFormatting.BLUE));
+                } else if (magLevel == 3) {
+                    components.add(new TranslatableComponent("tooltip.tacz.attachment.extended_mag_level_3").withStyle(ChatFormatting.LIGHT_PURPLE));
+                }
+            }
+
+            float adsAddendTime = data.getAdsAddendTime();
+            if (adsAddendTime > 0) {
+                components.add(new TranslatableComponent("tooltip.tacz.attachment.ads.increase").withStyle(ChatFormatting.RED));
+            } else if (adsAddendTime < 0) {
+                components.add(new TranslatableComponent("tooltip.tacz.attachment.ads.decrease").withStyle(ChatFormatting.GREEN));
+            }
+
+            float inaccuracyAddend = data.getInaccuracyAddend();
+            if (inaccuracyAddend > 0) {
+                components.add(new TranslatableComponent("tooltip.tacz.attachment.inaccuracy.increase").withStyle(ChatFormatting.RED));
+            } else if (inaccuracyAddend < 0) {
+                components.add(new TranslatableComponent("tooltip.tacz.attachment.inaccuracy.decrease").withStyle(ChatFormatting.GREEN));
+            }
+
+            RecoilModifier recoilModifier = data.getRecoilModifier();
+            if (recoilModifier != null) {
+                float pitch = recoilModifier.getPitch();
+                if (pitch > 0) {
+                    components.add(new TranslatableComponent("tooltip.tacz.attachment.pitch.increase").withStyle(ChatFormatting.RED));
+                } else if (pitch < 0) {
+                    components.add(new TranslatableComponent("tooltip.tacz.attachment.pitch.decrease").withStyle(ChatFormatting.GREEN));
+                }
+
+                float yaw = recoilModifier.getYaw();
+                if (yaw > 0) {
+                    components.add(new TranslatableComponent("tooltip.tacz.attachment.yaw.increase").withStyle(ChatFormatting.RED));
+                } else if (yaw < 0) {
+                    components.add(new TranslatableComponent("tooltip.tacz.attachment.yaw.decrease").withStyle(ChatFormatting.GREEN));
+                }
+            }
+        });
     }
 }
