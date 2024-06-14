5e60f603332ed6172aeb44c8795a23a66b2c0967
3TUSK <git@tritusk.info>
2024-06-13 20:57:43 -0700

    Change ServerGamePacketListenerImplMixin to use WrapOperation as a future-proof measure

--- src/main/java/com/tacz/guns/mixin/common/ServerGamePacketListenerImplMixin.java
+++ src/main/java/com/tacz/guns/mixin/common/ServerGamePacketListenerImplMixin.java
@@ -1,24 +1,25 @@
 package com.tacz.guns.mixin.common;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.tacz.guns.api.entity.IGunOperator;
 import com.tacz.guns.api.entity.ReloadState;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerGamePacketListenerImpl.class)
 public class ServerGamePacketListenerImplMixin {
-    @Redirect(method = "handlePlayerCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;setSprinting(Z)V"))
-    public void cancelSprintCommand(ServerPlayer player, boolean sprint) {
+    @WrapOperation(method = "handlePlayerCommand", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;setSprinting(Z)V"))
+    public void cancelSprintCommand(ServerPlayer player, boolean sprint, Operation<Void> original) {
         IGunOperator gunOperator = IGunOperator.fromLivingEntity(player);
         boolean isAiming = gunOperator.getSynIsAiming();
         ReloadState.StateType reloadStateType = gunOperator.getSynReloadState().getStateType();
         if (isAiming || (reloadStateType.isReloading() && !reloadStateType.isReloadFinishing())) {
-            player.setSprinting(false);
+            original.call(player, false);
         } else {
-            player.setSprinting(sprint);
+            original.call(player, sprint);
         }
     }
 }
