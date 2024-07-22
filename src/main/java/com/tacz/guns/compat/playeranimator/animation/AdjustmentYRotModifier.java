0a59cfa6e2679bacdfdc52e6b9e8d4d743ed09b1
tartaric_acid <baka943@qq.com>
2024-07-22 23:55:48 +0800

    完成趴下的动画设计 - 目前还存在部分问题

--- src/main/java/com/tacz/guns/compat/playeranimator/animation/AdjustmentYRotModifier.java
+++ src/main/java/com/tacz/guns/compat/playeranimator/animation/AdjustmentYRotModifier.java
@@ -4,6 +4,7 @@ import dev.kosmx.playerAnim.api.layered.modifier.AdjustmentModifier;
 import dev.kosmx.playerAnim.core.util.Vec3f;
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.player.Player;
 
 import java.util.Optional;
@@ -39,8 +40,14 @@ public class AdjustmentYRotModifier implements Function<String, Optional<Adjustm
         float pitch = Mth.wrapDegrees(xRot);
 
         return switch (partName) {
-            case "body" -> Optional.of(new AdjustmentModifier.PartModifier(new Vec3f(0, -yaw * Mth.DEG_TO_RAD, 0), Vec3f.ZERO));
-            case "head", "leftArm", "rightArm" -> Optional.of(new AdjustmentModifier.PartModifier(new Vec3f(pitch * Mth.DEG_TO_RAD, 0, 0), Vec3f.ZERO));
+            case "body" -> {
+                if (!player.isSwimming() && player.getPose() == Pose.SWIMMING) {
+                    yield Optional.of(new AdjustmentModifier.PartModifier(new Vec3f(0, 0, -yaw * Mth.DEG_TO_RAD), Vec3f.ZERO));
+                }
+                yield Optional.of(new AdjustmentModifier.PartModifier(new Vec3f(0, -yaw * Mth.DEG_TO_RAD, 0), Vec3f.ZERO));
+            }
+            case "head", "leftArm", "rightArm" ->
+                    Optional.of(new AdjustmentModifier.PartModifier(new Vec3f(pitch * Mth.DEG_TO_RAD, 0, 0), Vec3f.ZERO));
             default -> Optional.empty();
         };
     }
--- src/main/java/com/tacz/guns/compat/playeranimator/animation/AnimationManager.java
+++ src/main/java/com/tacz/guns/compat/playeranimator/animation/AnimationManager.java
@@ -58,7 +58,11 @@ public class AnimationManager {
     }
 
     public static void playLowerAnimation(AbstractClientPlayer player, ClientGunIndex gunIndex, float limbSwingAmount) {
-        // 如果玩家骑乘，不播放任何下半身动画
+        // 如果玩家趴下，不播放下半身动画
+        if (isPlayerLie(player)) {
+            return;
+        }
+        // 如果玩家骑乘
         if (player.getVehicle() != null) {
             playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOWER_ANIMATION, AnimationName.RIDE_LOWER);
             return;
@@ -97,25 +101,43 @@ public class AnimationManager {
         if (aimingProgress <= 0) {
             // 疾跑时播放的动画
             if (!isFlying(player) && player.isSprinting()) {
-                if (player.getPose() == Pose.CROUCHING) {
+                if (isPlayerLie(player)) {
+                    playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.LIE_MOVE);
+                } else if (player.getPose() == Pose.CROUCHING) {
                     playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.CROUCH_WALK_UPPER);
                 } else {
                     playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.RUN_UPPER);
                 }
                 return;
             }
+
             // 行走时的动画
             if (!isFlying(player) && limbSwingAmount > 0.05) {
-                if (player.getPose() == Pose.CROUCHING) {
+                if (isPlayerLie(player)) {
+                    playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.LIE_MOVE);
+                } else if (player.getPose() == Pose.CROUCHING) {
                     playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.CROUCH_WALK_UPPER);
                 } else {
-                    playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.RUN_UPPER);
+                    playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.WALK_UPPER);
                 }
                 return;
             }
-            playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.HOLD_UPPER);
+
+            if (isPlayerLie(player)) {
+                // 趴下时的动画
+                playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.LIE);
+            } else {
+                // 普通待命
+                playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.HOLD_UPPER);
+            }
         } else {
-            playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.AIM_UPPER);
+            if (isPlayerLie(player)) {
+                // 趴下时瞄准
+                playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.LIE_AIM);
+            } else {
+                // 普通瞄准
+                playLoopAnimation(player, gunIndex, PlayerAnimatorCompat.LOOP_UPPER_ANIMATION, AnimationName.AIM_UPPER);
+            }
         }
     }
 
@@ -137,6 +159,11 @@ public class AnimationManager {
             if (modifierLayer.getAnimation() instanceof KeyframeAnimationPlayer animationPlayer && animationPlayer.isActive()) {
                 Object extraDataName = animationPlayer.getData().extraData.get("name");
                 if (extraDataName instanceof String name && !animationName.equals(name)) {
+                    // 趴下动画先暂时不用过渡，会有问题
+                    if (name.startsWith(AnimationName.LIE)) {
+                        modifierLayer.setAnimation(new KeyframeAnimationPlayer(keyframeAnimation));
+                        return;
+                    }
                     AbstractFadeModifier fadeModifier = AbstractFadeModifier.standardFadeIn(8, Ease.INOUTSINE);
                     modifierLayer.replaceAnimationWithFade(fadeModifier, new KeyframeAnimationPlayer(keyframeAnimation));
                 }
@@ -187,6 +214,11 @@ public class AnimationManager {
         }
     }
 
+    private static boolean isPlayerLie(AbstractClientPlayer player) {
+        // MOJANG 的奇妙设计，趴下的姿势名称是 SWIMMING
+        return !player.isSwimming() && player.getPose() == Pose.SWIMMING;
+    }
+
     @SubscribeEvent
     public void onFire(GunShootEvent event) {
         if (event.getLogicalSide().isServer()) {
@@ -205,9 +237,17 @@ public class AnimationManager {
             IGunOperator operator = IGunOperator.fromLivingEntity(player);
             float aimingProgress = operator.getSynAimingProgress();
             if (aimingProgress <= 0) {
-                playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.NORMAL_FIRE_UPPER);
+                if (isPlayerLie(player)) {
+                    playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.LIE_NORMAL_FIRE);
+                } else {
+                    playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.NORMAL_FIRE_UPPER);
+                }
             } else {
-                playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.AIM_FIRE_UPPER);
+                if (isPlayerLie(player)) {
+                    playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.LIE_AIM_FIRE);
+                } else {
+                    playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.AIM_FIRE_UPPER);
+                }
             }
         });
     }
@@ -226,7 +266,13 @@ public class AnimationManager {
         if (iGun == null) {
             return;
         }
-        TimelessAPI.getClientGunIndex(iGun.getGunId(gunItemStack)).ifPresent(index -> playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.RELOAD_UPPER));
+        TimelessAPI.getClientGunIndex(iGun.getGunId(gunItemStack)).ifPresent(index -> {
+            if (isPlayerLie(player)) {
+                playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.LIE_RELOAD);
+            } else {
+                playOnceAnimation(player, index, PlayerAnimatorCompat.ONCE_UPPER_ANIMATION, AnimationName.RELOAD_UPPER);
+            }
+        });
     }
 
     @SubscribeEvent
