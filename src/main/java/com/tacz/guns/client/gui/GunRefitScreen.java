4f272d293ac29fc3f320f689036b58074df2cf52
tartaric_acid <baka943@qq.com>
2024-07-24 23:15:29 +0800

    完成属性界面的折叠按钮设计

--- src/main/java/com/tacz/guns/client/gui/GunRefitScreen.java
+++ src/main/java/com/tacz/guns/client/gui/GunRefitScreen.java
@@ -2,19 +2,17 @@ package com.tacz.guns.client.gui;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.tacz.guns.GunMod;
-import com.tacz.guns.api.TimelessAPI;
 import com.tacz.guns.api.item.IAttachment;
 import com.tacz.guns.api.item.IGun;
 import com.tacz.guns.api.item.attachment.AttachmentType;
 import com.tacz.guns.client.animation.screen.RefitTransform;
+import com.tacz.guns.client.gui.components.FlatColorButton;
 import com.tacz.guns.client.gui.components.refit.*;
 import com.tacz.guns.client.sound.SoundPlayManager;
-import com.tacz.guns.config.common.GunConfig;
 import com.tacz.guns.network.NetworkHandler;
 import com.tacz.guns.network.message.ClientMessageRefitGun;
 import com.tacz.guns.network.message.ClientMessageUnloadAttachment;
 import com.tacz.guns.sound.SoundManager;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.network.chat.Component;
@@ -22,14 +20,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.jetbrains.annotations.NotNull;
-
-import javax.annotation.Nonnull;
-import java.util.Map;
-import java.util.Objects;
 
 @Mod.EventBusSubscriber(value = Dist.CLIENT, modid = GunMod.MOD_ID)
 public class GunRefitScreen extends Screen {
@@ -41,6 +32,7 @@ public class GunRefitScreen extends Screen {
     public static final int ICON_UV_SIZE = 32;
     public static final int SLOT_SIZE = 18;
     private static final int INVENTORY_ATTACHMENT_SLOT_COUNT = 8;
+    private static boolean HIDE_GUN_PROPERTY_DIAGRAMS = false;
 
     private int currentPage = 0;
 
@@ -91,13 +83,23 @@ public class GunRefitScreen extends Screen {
         this.addAttachmentTypeButtons();
         // 添加可选配件列表
         this.addInventoryAttachmentButtons();
+        // 添加属性图隐藏按钮
+        if (HIDE_GUN_PROPERTY_DIAGRAMS) {
+            this.addRenderableWidget(new FlatColorButton(11, 11, 258, 16,
+                    Component.translatable("gui.tacz.gun_refit.property_diagrams.show"), b -> switchHideButton()));
+        } else {
+            this.addRenderableWidget(new FlatColorButton(11, 129, 258, 12,
+                    Component.translatable("gui.tacz.gun_refit.property_diagrams.hide"), b -> switchHideButton()));
+        }
     }
 
     @Override
     public void render(PoseStack poseStack, int mouseX, int mouseY, float pPartialTick) {
         super.render(poseStack, mouseX, mouseY, pPartialTick);
 
-        GunPropertyDiagrams.draw(this, poseStack, font, 11, 11);
+        if (!HIDE_GUN_PROPERTY_DIAGRAMS) {
+            GunPropertyDiagrams.draw(this, poseStack, font, 11, 11);
+        }
 
         this.renderables.stream().filter(w -> w instanceof IComponentTooltip).forEach(w -> ((IComponentTooltip) w)
                 .renderTooltip(component -> this.renderComponentTooltip(poseStack, component, mouseX, mouseY)));
@@ -227,5 +229,10 @@ public class GunRefitScreen extends Screen {
             this.addRenderableWidget(button);
             startX = startX - SLOT_SIZE;
         }
+    }
+
+    private void switchHideButton() {
+        HIDE_GUN_PROPERTY_DIAGRAMS = !HIDE_GUN_PROPERTY_DIAGRAMS;
+        this.init();
     }
 }
