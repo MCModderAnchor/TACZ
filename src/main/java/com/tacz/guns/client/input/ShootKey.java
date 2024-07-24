9aeebfd8568ddba15949caffec1319254b0214dd
tartaric_acid <baka943@qq.com>
2024-07-24 22:26:31 +0800

    为趴下增加手柄支持 - 现在还能自定义震动了(1.19.2/1.18.2 不支持此功能)

--- src/main/java/com/tacz/guns/client/input/ShootKey.java
+++ src/main/java/com/tacz/guns/client/input/ShootKey.java
@@ -3,6 +3,7 @@ package com.tacz.guns.client.input;
 import com.mojang.blaze3d.platform.InputConstants;
 import com.tacz.guns.api.TimelessAPI;
 import com.tacz.guns.api.client.gameplay.IClientPlayerGunOperator;
+import com.tacz.guns.api.entity.ShootResult;
 import com.tacz.guns.api.item.IGun;
 import com.tacz.guns.api.item.gun.FireMode;
 import com.tacz.guns.client.sound.SoundPlayManager;
@@ -57,14 +58,14 @@ public class ShootKey {
         }
     }
 
-    public static void autoShootController() {
+    public static boolean autoShootController() {
         if (!isInGame()) {
-            return;
+            return false;
         }
         Minecraft mc = Minecraft.getInstance();
         LocalPlayer player = mc.player;
         if (player == null || player.isSpectator()) {
-            return;
+            return false;
         }
         ItemStack mainHandItem = player.getMainHandItem();
         if (mainHandItem.getItem() instanceof IGun iGun) {
@@ -74,9 +75,10 @@ public class ShootKey {
                     .orElse(false);
             IClientPlayerGunOperator operator = IClientPlayerGunOperator.fromLocalPlayer(player);
             if (fireMode == FireMode.AUTO || isBurstAuto) {
-                operator.shoot();
+                return operator.shoot() == ShootResult.SUCCESS;
             }
         }
+        return false;
     }
 
     @SubscribeEvent
@@ -130,11 +132,10 @@ public class ShootKey {
                     .orElse(false);
             if (fireMode == FireMode.UNKNOWN) {
                 player.sendMessage(new TranslatableComponent("message.tacz.fire_select.fail"), Util.NIL_UUID);
-                return true;
+                return false;
             }
             if (fireMode == FireMode.SEMI || isBurstSemi) {
-                IClientPlayerGunOperator.fromLocalPlayer(player).shoot();
-                return true;
+                return IClientPlayerGunOperator.fromLocalPlayer(player).shoot() == ShootResult.SUCCESS;
             }
         }
         return false;
