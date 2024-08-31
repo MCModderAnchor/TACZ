17e5b2e54ab9b83870b6370ef26e141ad63a19e0
Fizeil <desyertro@gmail.com>
2024-08-31 15:37:38 +0800

    一处显示修复

--- src/main/java/com/tacz/guns/client/tooltip/ClientGunTooltip.java
+++ src/main/java/com/tacz/guns/client/tooltip/ClientGunTooltip.java
@@ -151,7 +151,7 @@ public class ClientGunTooltip implements ClientTooltipComponent {
 
             double damage = AttachmentDataUtils.getDamageWithAttachment(gun, gunData);
             MutableComponent value = new TextComponent(DAMAGE_FORMAT.format(damage)).withStyle(ChatFormatting.AQUA);
-            if (bulletData.getExplosionData() != null && AttachmentDataUtils.isExplodeEnabled(gun, gunData)) {
+            if (bulletData.getExplosionData() != null && (AttachmentDataUtils.isExplodeEnabled(gun, gunData) || bulletData.getExplosionData().isExplode())) {
                 value.append(" + ").append(DAMAGE_FORMAT.format(bulletData.getExplosionData().getDamage() * SyncConfig.DAMAGE_BASE_MULTIPLIER.get())).append(new TranslatableComponent("tooltip.tacz.gun.explosion"));
             }
             this.damage = new TranslatableComponent("tooltip.tacz.gun.damage").append(value);
