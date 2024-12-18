9014e63844fe3b8499531702a8948d143634f989
Fizeil <desyertro@gmail.com>
2024-12-18 18:04:48 +0800

    资源包界面调整

--- src/main/java/com/tacz/guns/resource/GunPackLoader.java
+++ src/main/java/com/tacz/guns/resource/GunPackLoader.java
@@ -117,7 +117,12 @@ public enum GunPackLoader implements RepositorySource {
             };
             extensionPacks.add(Pack.create("tacz_pack/"+gunPack.name(), true, () -> {
                 return new DelegatingPackResources(gunPack.name(), "TACZ Resources", new PackMetadataSection(Component.translatable("tacz.resources.modresources"),
-                        SharedConstants.getCurrentVersion().getPackVersion(packType)), List.of(packResources));
+                        SharedConstants.getCurrentVersion().getPackVersion(packType)), List.of(packResources)) {
+                    @Override
+                    public boolean isHidden() {
+                        return true;
+                    }
+                };
             }, pInfoFactory, Pack.Position.BOTTOM, PackSource.BUILT_IN));
         }
 
